269755898b14c586e11a1c9af6fb4d46bcbc14da
Akash2k20 <akashlearning2020@gmail.com>
2023-03-04 12:06:55 +0530

    idk

--- src/app/pages/TPO/internship/view-internship/view-internship.component.ts
+++ src/app/pages/TPO/internship/view-internship/view-internship.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-view-internship',
   templateUrl: './view-internship.component.html',
-  styleUrls: ['./view-internship.component.css']
 })
 export class ViewInternshipComponent {
 
